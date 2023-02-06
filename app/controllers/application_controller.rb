4771d2641b59eb16358c6984c9f65c6d6baa8143
fayob <adedejifavour6@gmail.com>
2023-02-06 17:23:59 +0100

    Fix application controller error

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -5,16 +5,8 @@ class ApplicationController < ActionController::Base
 
   protected
 
-      def configure_permitted_parameters
-          devise_parameter_sanitizer.for(:sign_up) { |u| u.permit(:name, :email, :password) }
-          devise_parameter_sanitizer.for(:account_update) { |u| u.permit(:name, :email, :password, :current_password) }
-      end
-
-  before_action :configure_permitted_parameters, if: :devise_controller?
-
-  protected
-    def configure_permitted_parameters
-      devise_parameter_sanitizer.permit(:sign_up) { |u| u.permit(:name, :email, :password) }
-      devise_parameter_sanitizer.permit(:account_update) { |u| u.permit(:name, :email, :password, :current_password) }
-    end
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.permit(:sign_up) { |u| u.permit(:name, :email, :password) }
+    devise_parameter_sanitizer.permit(:account_update) { |u| u.permit(:name, :email, :password, :current_password) }
+  end
 end
