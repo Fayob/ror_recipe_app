13f1a98434e470cf8566a2d9b563c002ea74d3c2
fayob <adedejifavour6@gmail.com>
2023-02-09 21:08:59 +0100

    Rename recipes controller to recipees

---
+++ app/controllers/recipees_controller.rb
@@ -0,0 +1,56 @@
+class RecipeesController < ApplicationController
+  skip_authorization_check only: [:public, :show]
+  load_and_authorize_resource except: [:public, :show]
+
+  def index
+    @recipes = Recipee.where(user: current_user).order(created_at: :desc)
+    # authorize! :read, @recipes.first
+  end
+
+  def public
+    @recipes = Recipee.includes(:user, :foods).where(public: true)
+  end
+
+  def show
+    @recipe = Recipee.includes(:user, :foods).find(params[:id])
+  end
+
+  def new
+    @recipe = Recipee.new
+  end
+
+  def create_recipe
+    @recipe = Recipee.new(user: current_user, name: recipe_param['name'],
+                          public: public?,
+                          description: recipe_param['description'],
+                          preparation_time: recipe_param['preparation_time'],
+                          cooking_time: recipe_param['cooking_time'])
+    # authorize! :create, @recipe
+    if @recipe.save
+      flash[:notice] = 'Recipe Added Successfully'
+      redirect_to recipees_path
+    else
+      render 'new'
+    end
+  end
+
+  def destroy
+    @recipe = Recipee.find(params[:id])
+    # authorize! :delete, @recipes
+    @recipe.destroy
+    respond_to do |format|
+      format.html { redirect_to recipees_path, notice: 'food was successfully deleted.' }
+      format.json { head :no_content }
+    end
+  end
+
+  private
+
+  def recipe_param
+    params.require(:recipee).permit(:name, :public, :description, :preparation_time, :cooking_time)
+  end
+
+  def public?
+    params['recipee']['public'] ? true : false
+  end
+end
